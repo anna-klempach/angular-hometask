6927744bae9b437a76e8140a5ccb1e8c7a1ecddb
Hanna Klempach <Hanna_Klempach@epam.com>
2019-09-20 11:04:46 +0300

    feat: generate core module and its components

---
+++ angular-hometask/src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { LogoComponent } from './logo/logo.component';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent, BreadcrumbsComponent, LogoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
