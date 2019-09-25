f613f9d549589a155ecead47e4c271aa957b13bf
Hanna Klempach <Hanna_Klempach@epam.com>
2019-09-25 17:15:14 +0300

    feat: implement tests for pick-a-boo component

--- angular-hometask/src/app/core/pick-a-boo/pick-a-boo.component.spec.ts
+++ angular-hometask/src/app/core/pick-a-boo/pick-a-boo.component.spec.ts
@@ -5,21 +5,31 @@ import { PickABooComponent } from './pick-a-boo.component';
 describe('PickABooComponent', () => {
   let component: PickABooComponent;
   let fixture: ComponentFixture<PickABooComponent>;
+  let element: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PickABooComponent ]
+      declarations: [PickABooComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PickABooComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get deleted on button click', () => {
+    const button = element.querySelector('button');
+    let emmitted = false;
+    component.deleteRequest.subscribe(() => emmitted = true);
+    button.click();
+    expect(emmitted).toBeTruthy();
   });
 });
