5d35118d63109b1943c1efc093a954e84a05e85f
Hanna Klempach <Hanna_Klempach@epam.com>
2019-09-25 17:38:57 +0300

    feat: implement tests for search bar component

--- angular-hometask/src/app/courses-page/search-bar/search-bar.component.spec.ts
+++ angular-hometask/src/app/courses-page/search-bar/search-bar.component.spec.ts
@@ -1,25 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchBarComponent } from './search-bar.component';
+import { DebugElement } from '@angular/core';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
   let fixture: ComponentFixture<SearchBarComponent>;
+  let elementDe: DebugElement;
+  let element: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchBarComponent ]
+      declarations: [SearchBarComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchBarComponent);
     component = fixture.componentInstance;
+    elementDe = fixture.debugElement;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should raise change searchValue property on input', () => {
+    component.searchValue = '';
+    const input = element.querySelector('input');
+    const event = new KeyboardEvent('keyup', {
+      view: window,
+      bubbles: true,
+      cancelable: true
+    });
+    input.value = '1';
+    input.dispatchEvent(event);
+    expect(component.searchValue).toBe('1');
+  });
+
+  it('should raise the event on search button click and pass element input value as its property', () => {
+    component.searchValue = 'ABC';
+    let expectedValue: string;
+    const button = element.querySelector('button');
+    component.searchValueChange.subscribe((inputValue: string) => expectedValue = inputValue);
+    button.click();
+    expect(expectedValue).toBe('ABC');
+    expect(component.searchValue).toBe('');
   });
 });
