cceb73cea1ef51707ea189bafb9dbdcde7e80da9
Hanna Klempach <Hanna_Klempach@epam.com>
2019-09-25 17:14:57 +0300

    feat: implement tests for app component

--- angular-hometask/src/app/app.component.spec.ts
+++ angular-hometask/src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -12,7 +13,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
 
@@ -28,10 +29,14 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-hometask');
   });
 
-  /* it('should render title', () => {
+  it('should generate pick-a-boo-component due to the status property', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-hometask app is running!');
-  }); */
+    expect(fixture.debugElement.query(By.css('.pab'))).toBeTruthy();
+    app.deleteItem();
+    fixture.detectChanges();
+    expect(app.pickABooActive).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('.pab'))).toBeFalsy();
+  });
 });
--- angular-hometask/src/app/core/breadcrumbs/breadcrumbs.component.spec.ts
+++ angular-hometask/src/app/core/breadcrumbs/breadcrumbs.component.spec.ts
@@ -5,6 +5,7 @@ import { BreadcrumbsComponent } from './breadcrumbs.component';
 describe('BreadcrumbsComponent', () => {
   let component: BreadcrumbsComponent;
   let fixture: ComponentFixture<BreadcrumbsComponent>;
+  let element: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +17,14 @@ describe('BreadcrumbsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BreadcrumbsComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should contain "Courses" in its text', () => {
+    expect(element.textContent).toBe('Courses');
   });
 });
