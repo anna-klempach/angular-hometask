1c8c0a02df19062cf2ef448f94999203f0b5263c
Hanna Klempach <Hanna_Klempach@epam.com>
2019-10-29 16:22:21 +0300

    fix: improve card styles

--- angular-hometask/src/app/modules/courses-page/components/courses-list-item/courses-list-item.component.ts
+++ angular-hometask/src/app/modules/courses-page/components/courses-list-item/courses-list-item.component.ts
@@ -1,18 +1,44 @@
-import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { CoursesListEntry } from '../../entities/classes/courses-list-entry';
 import { Router } from '@angular/router';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-courses-list-item',
   templateUrl: './courses-list-item.component.html',
   styleUrls: ['./courses-list-item.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    // animation triggers go here
+  ]
 })
 export class CoursesListItemComponent {
+  public focusable: boolean;
+  public focused: boolean;
+  private windowWidth: number;
   @Input() course: CoursesListEntry;
   @Input() index: number;
   @Output() deleteRequest: EventEmitter<number> = new EventEmitter<number>();
-  constructor(private router: Router) { }
+
+  private checkWindowSize() {
+    this.windowWidth = window.innerWidth;
+    if (this.windowWidth > 1000) {
+      this.focusable = true;
+      this.focused = false;
+    } else {
+      this.focusable = false;
+      this.focused = true;
+    }
+    console.log(this.windowWidth, this.focusable, this.focused);
+  }
+  constructor(private router: Router) {
+  }
 
   handleDeleteClick(): void {
     this.deleteRequest.emit(this.course.id);
