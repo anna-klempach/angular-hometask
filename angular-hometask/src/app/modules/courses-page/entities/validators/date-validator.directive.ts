1df5fc00041c6a208e8c57e92554e2b43d2006fb
Hanna Klempach <Hanna_Klempach@epam.com>
2019-11-14 11:24:52 +0300

    refactor: ommit using unnecessary const

--- angular-hometask/src/app/modules/courses-page/entities/validators/date-validator.directive.ts
+++ angular-hometask/src/app/modules/courses-page/entities/validators/date-validator.directive.ts
@@ -3,6 +3,5 @@ import { AbstractControl } from '@angular/forms';
 const dateReg = new RegExp('^([0]?[1-9]|[1|2][0-9]|[3][0|1])[/]([0]?[1-9]|[1][0-2])[/]([0-9]{4})$');
 
 export function dateValidator(control: AbstractControl): { [key: string]: any } | null {
-  const allowed = dateReg.test(control.value);
-  return allowed ? null : { forbiddenDateFormat: { value: control.value } };
+  return dateReg.test(control.value) ? null : { forbiddenDateFormat: { value: control.value } };
 }
