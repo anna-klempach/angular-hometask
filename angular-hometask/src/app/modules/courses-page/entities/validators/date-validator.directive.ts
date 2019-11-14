b18882532a3c3979559e1255e7aa24a61e3fca0b
Hanna Klempach <Hanna_Klempach@epam.com>
2019-11-14 11:25:48 +0300

    refactor: ommit return of null

--- angular-hometask/src/app/modules/courses-page/entities/validators/date-validator.directive.ts
+++ angular-hometask/src/app/modules/courses-page/entities/validators/date-validator.directive.ts
@@ -2,6 +2,6 @@ import { AbstractControl } from '@angular/forms';
 
 const dateReg = new RegExp('^([0]?[1-9]|[1|2][0-9]|[3][0|1])[/]([0]?[1-9]|[1][0-2])[/]([0-9]{4})$');
 
-export function dateValidator(control: AbstractControl): { [key: string]: any } | null {
+export function dateValidator(control: AbstractControl): { [key: string]: any } {
   return dateReg.test(control.value) ? null : { forbiddenDateFormat: { value: control.value } };
 }
