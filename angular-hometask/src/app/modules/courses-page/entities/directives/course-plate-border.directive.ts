6d5882784ead660a1ae8d72c3b651d7e3da42a32
Hanna Klempach <Hanna_Klempach@epam.com>
2019-11-14 15:30:44 +0300

    fix: add SimpleChange check

--- angular-hometask/src/app/modules/courses-page/entities/directives/course-plate-border.directive.ts
+++ angular-hometask/src/app/modules/courses-page/entities/directives/course-plate-border.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
 
 @Directive({
   selector: '[appCoursePlateBorder]'
@@ -10,14 +10,16 @@ export class CoursePlateBorderDirective implements OnChanges {
   constructor(public el: ElementRef) {
   }
 
-  ngOnChanges() {
-    const currentDate = Date.now() / 1000 / 60 / 60 / 24;
-    const currCreationDate = new Date(this.creationDate).valueOf() / 1000 / 60 / 60 / 24;
-    if (currCreationDate < currentDate && currCreationDate >= currentDate - 14) {
-      this.el.nativeElement.firstChild.children[0].style.border = '0.1rem solid rgb(77, 201, 160)';
-    } else if (currCreationDate > currentDate) {
-      this.el.nativeElement.firstChild.children[0].style.border = '0.1rem solid #374785';
+  ngOnChanges(changes: SimpleChanges) {
+    const change = changes.creationDate;
+    if (change.currentValue !== change.previousValue) {
+      const currentDate = Date.now() / 1000 / 60 / 60 / 24;
+      const currCreationDate = new Date(this.creationDate).valueOf() / 1000 / 60 / 60 / 24;
+      if (currCreationDate < currentDate && currCreationDate >= currentDate - 14) {
+        this.el.nativeElement.firstChild.children[0].style.border = '0.1rem solid rgb(77, 201, 160)';
+      } else if (currCreationDate > currentDate) {
+        this.el.nativeElement.firstChild.children[0].style.border = '0.1rem solid #374785';
+      }
     }
   }
-
 }
